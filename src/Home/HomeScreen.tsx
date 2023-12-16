1f265ce98796591d22de8fd44d213f84dced3660
Weronika <weronikamazurczak202@gmail.com>
2023-12-16 02:18:40 +0100

    HomeScreen styles

--- src/Home/HomeScreen.tsx
+++ src/Home/HomeScreen.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { View, ScrollView } from "@gluestack-ui/themed";
+import { Image, ScrollView, Text } from "@gluestack-ui/themed";
+import { LinearGradient } from "expo-linear-gradient";
 import styles from "./HomeStyle";
 
 export default function HomeScreen() {
   return (
-    <View style = {styles.mainHomeScreenView}>
-      <ScrollView style={styles.homeScreenScrollView}></ScrollView>
-    </View>
+    <LinearGradient
+      colors={["#00B4DB", "#0083B0"]}
+      style={styles.firstPlanBackground}
+    >
+      <LinearGradient
+        colors={["#6dd5ed", "#2193b0"]}
+        style={styles.secondPlanBackground}
+      >
+        <ScrollView>
+          <Text style={styles.localizationTextStyle}>Location</Text>
+          <Text style={styles.dateTextStyle}>Tuesday 12 December</Text>
+          <Text style={styles.temperatureTextStyle}> 1°C </Text>
+        </ScrollView>
+      </LinearGradient>
+    </LinearGradient>
   );
 }
--- src/Home/HomeStyle.tsx
+++ src/Home/HomeStyle.tsx
@@ -2,16 +2,40 @@ import { StyleSheet } from "react-native";
 import React from "react";
 
 const styles = StyleSheet.create({
-    mainHomeScreenView:{
-flex:1,
-    },
-    homeScreenScrollView:{
-position: "absolute",
-width: "80%",
-height: "100%",
-margin: "10%",
-backgroundColor: "red",
-    },
+  firstPlanBackground: {
+    height: "100%",
+    alignItems: "center",
+  },
+  secondPlanBackground: {
+    position: "absolute",
+    width: "80%",
+    height: "100%",
+    marginTop: "10%",
+    borderRadius: 50,
+  },
+  localizationTextStyle: {
+    textAlign: "center",
+    justifyContent: "center",
+    color: "white",
+    fontSize: 40,
+    marginTop: "10%",
+    paddingTop: "10%",
+  },
+  dateTextStyle: {
+    textAlign: "center",
+    justifyContent: "center",
+    color: "white",
+    fontSize: 20,
+    marginTop: "3%",
+  },
+  temperatureTextStyle: {
+    textAlign: "center",
+    justifyContent: "center",
+    color: "white",
+    fontSize: 50,
+    marginTop: "3%",
+    padding: "10%",
+  },
 });
 
 export default styles;
