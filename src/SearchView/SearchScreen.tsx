219b77a48c551405bfb477c3af77da50a2763b7d
Weronika <weronikamazurczak202@gmail.com>
2023-12-19 23:46:35 +0100

    SearchViewStyle

--- src/SearchView/SearchScreen.tsx
+++ src/SearchView/SearchScreen.tsx
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Image,
+  ScrollView,
+  Text,
+  Input,
+  InputField,
+} from "@gluestack-ui/themed";
+import React from "react";
+import styles from "./SearchViewStyle";
 
 export default function SearchScreen() {
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={styles.mainSearchScreenView}>
+      <Image
+        blurRadius={50}
+        style={styles.background}
+        source={require("../../Images/wave_background.png")}
+        alt="cloudy"
+      />
+      <View style={styles.searchInputView}>
+        <Input
+          style={styles.searchInput}
+          variant="outline"
+          size="lg"
+          isDisabled={false}
+          isInvalid={false}
+          isReadOnly={false}
+        >
+          <InputField style={styles.searchInputText} placeholder="Search city" />
+        </Input>
+      </View>
     </View>
-  )
+  );
 }
