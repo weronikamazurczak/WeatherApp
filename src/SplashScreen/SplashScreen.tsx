60331aacf9e055eca2354836ffb9f02c893827f4
Weronika <weronikamazurczak202@gmail.com>
2023-12-21 22:51:48 +0100

    connect weather api

--- src/SplashScreen/SplashScreen.tsx
+++ src/SplashScreen/SplashScreen.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import styles from "./SplashScreenStyle";
 import { View, Text } from "@gluestack-ui/themed";
-
-
+import { ApiObject } from "./WeatherApiCredentials";
 
 export default function SplashScreen({ navigation }: any) {
-  useEffect(() => {
-    setTimeout(() => {
-      navigation.navigate("TabNavigationScreens");
-    }, 5000);
-  });
 
+  useEffect(() => { 
+    (async () => {
+      await getWeatherData({ navigation }); 
+    })()
+  },[]);
   return (
     <View style={styles.mainSplashScreenView}>
       <Text style={styles.weatherText}>Weather</Text>
-
-
     </View>
   );
 }
+
+async function getWeatherData({ navigation }: any) {
+  const link = `https://api.openweathermap.org/data/2.5/weather?lat=50.88599804710416&lon=20.655638450157436&appid=${ApiObject.key}`;
+  const response = await fetch(link);
+  console.log(await response.json());
+  setTimeout(() => {
+    navigation.navigate("TabNavigationScreens");
+  }, 5000);
+}
+
+
